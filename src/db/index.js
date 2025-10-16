56ccdb5e1996a3dd2c034e424e9b70b79cb4eeb0
mariaruban80 <maria.ruban80@gmail.com>
2025-10-16 15:38:01 +0100

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -6,10 +6,33 @@ dotenv.config();
 
 let db;
 
-if (process.env.DB_TYPE === 'supabase') {
-  db = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-  console.log("✅ Connected to Supabase");
-} else if (process.env.DB_TYPE === 'oracle') {
+if (process.env.DB_TYPE === "supabase") {
+  const { data, error } = await db
+    .from("users")
+    .insert([{
+      Id: crypto.randomUUID(),    // generate a unique Id
+      name,
+      email,
+      password: hashed,
+      role: "user",               // default role
+      created_at: new Date().toISOString()
+    }])
+    .select();
+
+  if (error) {
+    console.error("Supabase insert error:", error);
+    return res.status(500).json({ error: "Supabase insert failed: " + error.message });
+  }
+
+  console.log("User inserted:", data[0]);
+  res.status(201).json({ message: "User registered successfully", user: data[0] });
+}
+
+//if (process.env.DB_TYPE === 'supabase') {
+ // db = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+  //console.log("✅ Connected to Supabase");
+//} 
+else if (process.env.DB_TYPE === 'oracle') {
   db = await oracledb.getConnection({
     user: process.env.ORACLE_USER,
     password: process.env.ORACLE_PASS,
